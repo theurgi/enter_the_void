edd5dbf09cddb19098122d734e709badd2419d1d
theurgi <58859663+theurgi@users.noreply.github.com>
2023-08-12 15:31:38 -0400

    create staging dir and improve naming

--- scripts/nvidia_340.sh
+++ scripts/nvidia_340.sh
@@ -8,44 +8,44 @@
 
 set -e
 
-# Base directory and driver details
+# Base directory
 BASE_DIR=$(dirname "$0")
+
+# Create a staging directory to store and process the drivers and patch files.
+STAGING_DIR="$BASE_DIR/staging"
+mkdir -p "$STAGING_DIR"
+
+# Source URLs
 DRIVER_URL="https://us.download.nvidia.com/XFree86/Linux-x86_64/340.108/NVIDIA-Linux-x86_64-340.108.run"
-UNPATCHED_DRIVER=$(basename "$DRIVER_URL")
-EXTRACTED_UNPATCHED_DRIVER="${UNPATCHED_DRIVER%.run}"
+AUR_PATCHES_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/nvidia-340xx.tar.gz"
 
 # Expected blake2 hash of the NVIDIA driver for verification
-EXPECTED_B2SUM="890d00ff2d1d1a602d7ce65e62d5c3fdb5d9096b61dbfa41e3348260e0c0cc068f92b32ee28a48404376e7a311e12ad1535c68d89e76a956ecabc4e452030f15"
+EXPECTED_DRIVER_HASH="890d00ff2d1d1a602d7ce65e62d5c3fdb5d9096b61dbfa41e3348260e0c0cc068f92b32ee28a48404376e7a311e12ad1535c68d89e76a956ecabc4e452030f15"
 
-# Determine the actual kernel version based on LINUX_VERSION
+# Set paths/names for driver assets
+UNPATCHED_DRIVER="$STAGING_DIR/$(basename "$DRIVER_URL")"
+DRIVER_EXTRACTION_DIR="$STAGING_DIR/${UNPATCHED_DRIVER%.run}"
+PATCHED_DRIVER="$DRIVER_EXTRACTION_DIR-patched-for-kernel-$KERNEL_VERSION.run"
+PATCHED_DRIVER_DESCRIPTION="NVIDIA driver 340.108 patched for kernel $KERNEL_VERSION"
+
+# Determine the numeric kernel version for named instances of LINUX_VERSION
 if [ "$LINUX_VERSION" = "linux" ] || [ "$LINUX_VERSION" = "linux-lts" ] || [ "$LINUX_VERSION" = "linux-mainline" ]; then
 	KERNEL_VERSION=$(xbps-query -R "$LINUX_VERSION" | grep -oP "pkgver: $LINUX_VERSION-\K[^_]+" | awk -F. '{print $1"."$2}')
 else
 	KERNEL_VERSION=$(echo "$LINUX_VERSION" | sed 's/linux//')
 fi
 
-PATCH_FILE="NVIDIA-340xx/kernel-$KERNEL_VERSION.patch"
-
-# Check if a patch exists for this kernel version
-if [ ! -f "$PATCH_FILE" ]; then
-	echo "Error: No patch available for kernel version $KERNEL_VERSION"
-	exit 1
-fi
-
-PATCHED_DRIVER="$EXTRACTED_UNPATCHED_DRIVER-patched-kernel-$KERNEL_VERSION.run"
-PATCHED_DRIVER_DESCRIPTION="NVIDIA driver 340.108 patched for kernel $KERNEL_VERSION"
-
-# Install dependencies
+# Ensure the dependencies of this script are satisfied.
 declare -a dev_dependencies=("patch" "wget")
 xbps-install -Sy "${dev_dependencies[@]}"
 
+# Ensure the dependencies of the Nvidia drivers exist on the Void installation.
 declare -a installation_dependencies=("libglvnd" "libvdpau" "libglapi")
 xbps-install -Sy -r $SYSTEM_ROOT "${installation_dependencies[@]}"
 
-# Change to the base directory
-cd "$BASE_DIR"
+# Change to the staging directory to process and patch the drivers.
+cd "$STAGING_DIR"
 
-# Download the driver if it's not already present
 if [ ! -f "$UNPATCHED_DRIVER" ]; then
 	echo "Downloading the NVIDIA driver..."
 	wget "$DRIVER_URL"
@@ -53,28 +53,47 @@ fi
 
 # Verify the driver's integrity
 echo "Verifying driver integrity..."
-B2SUM_HASH=$(b2sum "$UNPATCHED_DRIVER" | cut -f 1 -d " ")
-if [ "$EXPECTED_B2SUM" != "$B2SUM_HASH" ]; then
+DRIVER_HASH=$(b2sum "$UNPATCHED_DRIVER" | cut -f 1 -d " ")
+if [ "$EXPECTED_DRIVER_HASH" != "$DRIVER_HASH" ]; then
 	echo "Error: The driver's hash doesn't match the expected value. Aborting."
 	exit 1
 fi
 
-# Make driver executable
-chmod +x "$UNPATCHED_DRIVER"
+# Download and extract the AUR patches to the staging directory.
+wget -O "$STAGING_DIR/nvidia-340xx.tar.gz" "$AUR_PATCHES_URL"
+tar xvf "$STAGING_DIR/nvidia-340xx.tar.gz" -C "$STAGING_DIR"
 
-# Extract
+# Extract the NVIDIA driver
+chmod +x "$UNPATCHED_DRIVER"
 "$UNPATCHED_DRIVER" --extract-only
 
-# Change to the extracted directory
-cd "$EXTRACTED_UNPATCHED_DRIVER"
+# Incrementally apply patches up to LINUX_VERSION
+cd "$DRIVER_EXTRACTION_DIR"
+for patch_file in $(ls $STAGING_DIR/nvidia-340xx/0*.patch | sort); do
+	# Extract version number from the patch filename
+	patch_version=${patch_file##*-}
+	patch_version=${patch_version%.patch}
 
-# Apply the patch to the extracted contents
-echo "Patching the driver for kernel $KERNEL_VERSION..."
-patch -Np1 -i "../$PATCH_FILE"
+	# Check if patch_version is less than or equal to KERNEL_VERSION
+	if [[ $(echo -e "$KERNEL_VERSION\n$patch_version" | sort -V | head -n1) == $patch_version ]]; then
+		echo "Applying patch for kernel $patch_version..."
+		patch -Np1 <"$patch_file"
+	fi
+
+	# If the patch version equals the kernel version, break out of loop after applying
+	if [[ $patch_version == $KERNEL_VERSION ]]; then
+		break
+	fi
+done
 
 # Repackage the patched driver
 echo "Repackaging the patched driver..."
-./makeself.sh --target-os Linux --target-arch x86_64 "$EXTRACTED_UNPATCHED_DRIVER" "$PATCHED_DRIVER" "$PATCHED_DRIVER_DESCRIPTION" ./nvidia-installer
+./makeself.sh \
+	--target-os Linux \
+	--target-arch x86_64 \
+	"$DRIVER_EXTRACTION_DIR" \
+	"$PATCHED_DRIVER" "$PATCHED_DRIVER_DESCRIPTION" \
+	./nvidia-installer
 
 # Move the repackaged driver to $SYSTEM_ROOT/tmp
 echo "Moving repackaged driver to $SYSTEM_ROOT/tmp..."
@@ -95,6 +114,7 @@ echo "Installing the repackaged NVIDIA driver..."
 chroot "$SYSTEM_ROOT" sh "/tmp/$PATCHED_DRIVER"
 
 # Remove the installer and extracted folder after completion
+# TODO rm staging directory
 rm -rf $SYSTEM_ROOT/tmp/$PATCHED_DRIVER
 
 echo "Driver installation completed!"
