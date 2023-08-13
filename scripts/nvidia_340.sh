b9513b6f3cf6d9f0559139ee139b39c3fac11225
theurgi <58859663+theurgi@users.noreply.github.com>
2023-08-12 21:06:39 -0400

    fix bugs

--- scripts/nvidia_340.sh
+++ scripts/nvidia_340.sh
@@ -9,7 +9,7 @@
 set -e
 
 # Base directory
-BASE_DIR=$(dirname "$0")
+export BASE_DIR=$(realpath $(dirname "$0"))
 
 # Create a staging directory to store and process the drivers and patch files
 STAGING_DIR="$BASE_DIR/staging"
@@ -23,10 +23,8 @@ AUR_PATCHES_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/nvidia-340xx.ta
 EXPECTED_DRIVER_HASH="890d00ff2d1d1a602d7ce65e62d5c3fdb5d9096b61dbfa41e3348260e0c0cc068f92b32ee28a48404376e7a311e12ad1535c68d89e76a956ecabc4e452030f15"
 
 # Set paths/names for driver assets
-UNPATCHED_DRIVER="$STAGING_DIR/$(basename "$DRIVER_URL")"
-DRIVER_EXTRACTION_DIR="$STAGING_DIR/${UNPATCHED_DRIVER%.run}"
-PATCHED_DRIVER="$DRIVER_EXTRACTION_DIR-patched-for-kernel-$KERNEL_VERSION.run"
-PATCHED_DRIVER_DESCRIPTION="NVIDIA driver 340.108 patched for kernel $KERNEL_VERSION"
+UNPATCHED_DRIVER="$STAGING_DIR/NVIDIA-Linux-x86_64-340.108.run"
+DRIVER_EXTRACTION_DIR="$STAGING_DIR/NVIDIA-Linux-x86_64-340.108"
 
 # Determine the numeric kernel version for named instances of LINUX_VERSION
 if [ "$LINUX_VERSION" = "linux" ] || [ "$LINUX_VERSION" = "linux-lts" ] || [ "$LINUX_VERSION" = "linux-mainline" ]; then
@@ -34,6 +32,10 @@ if [ "$LINUX_VERSION" = "linux" ] || [ "$LINUX_VERSION" = "linux-lts" ] || [ "$L
 else
 	KERNEL_VERSION=$(echo "$LINUX_VERSION" | sed 's/linux//')
 fi
+
+# Set path, name, and description for patched driver assets
+PATCHED_DRIVER="$DRIVER_EXTRACTION_DIR-patched-for-kernel-$KERNEL_VERSION.run"
+PATCHED_DRIVER_DESCRIPTION="NVIDIA driver 340.108 patched for kernel $KERNEL_VERSION"
 
 # Ensure the dependencies of this script are satisfied
 declare -a DEV_PACKAGES=("patch" "wget")
@@ -62,11 +64,11 @@ tar xvf "$STAGING_DIR/nvidia-340xx.tar.gz" -C "$STAGING_DIR"
 
 # Extract the NVIDIA driver
 chmod +x "$UNPATCHED_DRIVER"
-"$UNPATCHED_DRIVER" --extract-only
+"$UNPATCHED_DRIVER" --extract-only --target "$DRIVER_EXTRACTION_DIR"
 
 # Incrementally apply patches up to LINUX_VERSION
 pushd "$DRIVER_EXTRACTION_DIR"
-for patch_file in $(ls "$STAGING_DIR/nvidia-340xx/0*.patch" | sort); do
+for patch_file in $(ls $STAGING_DIR/nvidia-340xx/0*.patch | sort); do
 	# Extract version number from the patch filename
 	patch_version=${patch_file##*-}
 	patch_version=${patch_version%.patch}
@@ -106,7 +108,7 @@ chroot "$SYSTEM_ROOT" rmmod nouveau || echo "Nouveau module not loaded."
 
 # Run the repackaged driver installer from chroot environment
 echo "Installing the repackaged NVIDIA driver..."
-chroot "$SYSTEM_ROOT" sh "/tmp/$(basename "$PATCHED_DRIVER")"
+chroot "$SYSTEM_ROOT" sh "/tmp/$(basename "$PATCHED_DRIVER")" --silent
 
 # Remove the installer and extracted folder after completion
 echo "Cleaning up temporary installation assets..."
