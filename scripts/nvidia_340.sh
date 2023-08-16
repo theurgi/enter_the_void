1d46c1fe254398a635aa458f5c31ccef541e5404
theurgi <58859663+theurgi@users.noreply.github.com>
2023-08-16 14:54:21 -0400

    refactor: install w/o the nvidia '.run' installer

--- scripts/nvidia_340.sh
+++ scripts/nvidia_340.sh
@@ -5,6 +5,10 @@
 #
 # - LINUX_VERSION
 # - SYSTEM_ROOT
+#
+# Script Assumptions:
+# - x86_64 architecture
+# - 64-bit driver installation
 
 set -e
 
@@ -15,12 +19,14 @@ export BASE_DIR=$(realpath $(dirname "$0"))
 STAGING_DIR="$BASE_DIR/staging"
 mkdir -p "$STAGING_DIR"
 
-# Source URLs
-DRIVER_URL="https://us.download.nvidia.com/XFree86/Linux-x86_64/340.108/NVIDIA-Linux-x86_64-340.108.run"
-AUR_PATCHES_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/nvidia-340xx.tar.gz"
-
-# Expected blake2 hash of the NVIDIA driver for verification
+# Source package
+DRIVER_VERSION="340.108"
+PKG="NVIDIA-Linux-x86_64-${DRIVER_VERSION}-no-compat32"
+DRIVER_URL="https://us.download.nvidia.com/XFree86/Linux-x86_64/${DRIVER_VERSION}/${PKG}.run"
 EXPECTED_DRIVER_HASH="890d00ff2d1d1a602d7ce65e62d5c3fdb5d9096b61dbfa41e3348260e0c0cc068f92b32ee28a48404376e7a311e12ad1535c68d89e76a956ecabc4e452030f15"
+
+# Source patch files
+AUR_PATCHES_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/nvidia-340xx.tar.gz"
 
 # Set paths/names for driver assets
 UNPATCHED_DRIVER="$STAGING_DIR/NVIDIA-Linux-x86_64-340.108.run"
@@ -38,7 +44,7 @@ PATCHED_DRIVER="$DRIVER_EXTRACTION_DIR-patched-for-kernel-$KERNEL_VERSION.run"
 PATCHED_DRIVER_DESCRIPTION="NVIDIA driver 340.108 patched for kernel $KERNEL_VERSION"
 
 # Ensure the dependencies of this script are satisfied
-declare -a DEV_PACKAGES=("patch" "wget")
+declare -a DEV_PACKAGES=("patch" "wget" "tar")
 xbps-install -Sy "${DEV_PACKAGES[@]}"
 
 # Ensure the dependencies of the Nvidia drivers exist on the Void installation
@@ -66,8 +72,10 @@ tar xvf "$STAGING_DIR/nvidia-340xx.tar.gz" -C "$STAGING_DIR"
 chmod +x "$UNPATCHED_DRIVER"
 "$UNPATCHED_DRIVER" --extract-only --target "$DRIVER_EXTRACTION_DIR"
 
-# Incrementally apply patches up to LINUX_VERSION
+# Change into the driver directory
 pushd "$DRIVER_EXTRACTION_DIR"
+
+# Incrementally apply patches up to LINUX_VERSION
 for patch_file in $(ls $STAGING_DIR/nvidia-340xx/0*.patch | sort); do
 	# Extract version number from the patch filename
 	patch_version=${patch_file##*-}
@@ -79,25 +87,162 @@ for patch_file in $(ls $STAGING_DIR/nvidia-340xx/0*.patch | sort); do
 		patch -Np1 <"$patch_file"
 	fi
 
+	# TODO can simplify or remove this
 	# If the patch version equals the kernel version, break out of loop after applying
 	if [[ $patch_version == $KERNEL_VERSION ]]; then
 		break
 	fi
 done
+
+# ──────────────────────────────────────────────────────────────────────────────
+# ───────────────────────────── Start Installation ─────────────────────────────
+# ──────────────────────────────────────────────────────────────────────────────
+
+# X driver
+install -m 755 nvidia_drv.so "$SYSTEM_ROOT/usr/lib/xorg/modules/drivers"
+
+# GLX extension module for X
+install -m 755 "libglx.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/nvidia/xorg/"
+ln -sf "libglx.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/nvidia/xorg/libglx.so.1"
+ln -sf "libglx.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/nvidia/xorg/libglx.so"
+
+# # TODO install/write_file to /etc/X11/xorg.conf.d/20-nvidia.conf
+# Section "Files"
+#   ModulePath   "/usr/lib64/nvidia/xorg"
+#   ModulePath   "/usr/lib64/xorg/modules"
+# EndSection
+
+# Section "Device"
+#   Identifier "Nvidia Card"
+#   Driver "nvidia"
+#   VendorName "NVIDIA Corporation"
+# EndSection
+
+# Section "ServerFlags"
+#   Option "IgnoreABI" "1"
+# EndSection
+
+# OpenGL libraries (GL, EGL and GLES)
+install -m 755 "libGL.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libGL.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libGL.so.1"
+
+install -m 755 "libEGL.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libEGL.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libEGL.so.1"
+
+install -m 755 "libGLESv1_CM.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libGLESv1_CM.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libGLESv1_CM.so.1"
+
+install -m 755 "libGLESv2.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libGLESv2.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libGLESv2.so.2"
+
+# Some programs link to these libglvnd links
+ln -sf "libGL.so.1" "$SYSTEM_ROOT/usr/lib/libOpenGL.so.0"
+ln -sf "libGL.so.1" "$SYSTEM_ROOT/usr/lib/libGLX.so.0"
+
+# OpenGL core library
+install -m 755 "libnvidia-glcore.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-glcore.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-glcore.so"
+
+# VDPAU
+install -m 755 "libvdpau_nvidia.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/vdpau/"
+ln -sf "libvdpau_nvidia.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/vdpau/libvdpau_nvidia.so"
+ln -sf "libvdpau_nvidia.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/vdpau/libvdpau_nvidia.so.1"
+
+# misc libraries
+install -m 755 "tls/libnvidia-tls.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-tls.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-tls.so"
+
+install -m 755 "libnvidia-cfg.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-cfg.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-cfg.so"
+ln -sf "libnvidia-cfg.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-cfg.so.1"
+
+install -m 755 "libnvidia-ml.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-ml.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-ml.so"
+ln -sf "libnvidia-ml.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-ml.so.1"
+
+install -m 755 "libnvidia-encode.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-encode.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-encode.so"
+ln -sf "libnvidia-encode.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-encode.so.1"
+
+install -m 755 "libnvidia-ifr.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-ifr.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-ifr.so"
+ln -sf "libnvidia-ifr.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-ifr.so.1"
+
+install -m 755 "libnvidia-fbc.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvidia-fbc.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-fbc.so"
+ln -sf "libnvidia-fbc.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvidia-fbc.so.1"
+
+install -m 755 "libnvidia-glsi.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+
+# CUDA
+install -m 755 "nvidia-cuda-mps-control" "$SYSTEM_ROOT/usr/bin/"
+install -m 755 "nvidia-cuda-mps-server" "$SYSTEM_ROOT/usr/bin/"
+gzip -c "nvidia-cuda-mps-control.1" >"$SYSTEM_ROOT/usr/share/man/man1/nvidia-cuda-mps-control.1.gz"
+
+install -m 755 "libcuda.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libcuda.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libcuda.so"
+ln -sf "libcuda.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libcuda.so.1"
+
+install -m 755 "libnvcuvid.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/"
+ln -sf "libnvcuvid.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvcuvid.so"
+ln -sf "libnvcuvid.so.${DRIVER_VERSION}" "$SYSTEM_ROOT/usr/lib/libnvcuvid.so.1"
+
+# nvidia-xconfig
+install -m 755 "nvidia-xconfig" "$SYSTEM_ROOT/usr/bin/"
+gzip -c "nvidia-xconfig.1" >"$SYSTEM_ROOT/usr/share/man/man1/nvidia-xconfig.1.gz"
+
+# nvidia-settings
+install -m 755 "nvidia-settings" "$SYSTEM_ROOT/usr/bin/"
+gzip -c "nvidia-settings.1" >"$SYSTEM_ROOT/usr/share/man/man1/nvidia-settings.1.gz"
+install -m 644 "nvidia-settings.desktop" "$SYSTEM_ROOT/usr/share/applications/"
+install -m 644 "nvidia-settings.png" "$SYSTEM_ROOT/usr/share/pixmaps/"
+sed -e 's:__UTILS_PATH__:/usr/bin:' \
+	-e 's:__PIXMAP_PATH__:/usr/share/pixmaps:' \
+	-i "$SYSTEM_ROOT/usr/share/applications/nvidia-settings.desktop"
+
+# nvidia-bug-report
+install -m 755 "nvidia-bug-report.sh" "$SYSTEM_ROOT/usr/bin/nvidia-bug-report"
+install -m 755 "nvidia-debugdump" "$SYSTEM_ROOT/usr/bin/"
+
+# nvidia-smi
+install -m 755 "nvidia-smi" "$SYSTEM_ROOT/usr/bin/"
+gzip -c "nvidia-smi.1" >"$SYSTEM_ROOT/usr/share/man/man1/nvidia-smi.1.gz"
+
+# License and documentation.
+install -m 644 "LICENSE" "$SYSTEM_ROOT/usr/share/licenses/NVIDIA/"
+install -m 644 "README.txt" "$SYSTEM_ROOT/usr/share/doc/NVIDIA/README"
+install -m 644 "NVIDIA_Changelog" "$SYSTEM_ROOT/usr/share/doc/NVIDIA/"
+
+# opencl pkg
+install -m 644 "nvidia.icd" "$SYSTEM_ROOT/etc/OpenCL/vendors"
+
+install -m 755 "libnvidia-compiler.so.$DRIVER_VERSION" "$SYSTEM_ROOT/usr/lib"
+ln -sf "libnvidia-compiler.so.$DRIVER_VERSION" "$SYSTEM_ROOT/usr/lib/libnvidia-compiler.so"
+ln -sf "libnvidia-compiler.so.$DRIVER_VERSION" "$SYSTEM_ROOT/usr/lib/libnvidia-compiler.so.1"
+
+install -m 755 "libnvidia-opencl.so.$DRIVER_VERSION" "$SYSTEM_ROOT/usr/lib"
+ln -sf "libnvidia-opencl.so.$DRIVER_VERSION" "$SYSTEM_ROOT/usr/lib/libnvidia-opencl.so"
+ln -sf "libnvidia-opencl.so.$DRIVER_VERSION" "$SYSTEM_ROOT/usr/lib/libnvidia-opencl.so.1"
+
+# DKMS
+mkdir -p "$SYSTEM_ROOT/usr/src/nvidia-$DRIVER_VERSION"
+cat "kernel/uvm/dkms.conf.fragment" >>"kernel/dkms.conf"
+cp -r "kernel/*" "$SYSTEM_ROOT/usr/src/nvidia-$DRIVER_VERSION"
+
+# Load the kernel modules at system boot
+mkdir -p "$SYSTEM_ROOT/usr/lib/modules-load.d"
+echo "nvidia" >"$SYSTEM_ROOT/usr/lib/modules-load.d/nvidia.conf"
+chmod 644 "$SYSTEM_ROOT/usr/lib/modules-load.d/nvidia.conf"
+
+echo "nvidia-uvm" >"$SYSTEM_ROOT/usr/lib/modules-load.d/nvidia-uvm.conf"
+chmod 644 "$SYSTEM_ROOT/usr/lib/modules-load.d/nvidia-uvm.conf"
+
+# ──────────────────────────────────────────────────────────────────────────────
+# ────────────────────────────── End Installation ──────────────────────────────
+# ──────────────────────────────────────────────────────────────────────────────
+
+# Exit the driver directory
 popd
-
-# Repackage the patched driver
-echo "Repackaging the patched driver..."
-"$DRIVER_EXTRACTION_DIR/makeself.sh" \
-	--target-os Linux \
-	--target-arch x86_64 \
-	"$DRIVER_EXTRACTION_DIR" \
-	"$PATCHED_DRIVER" "$PATCHED_DRIVER_DESCRIPTION" \
-	./nvidia-installer
-
-# Move the repackaged driver to $SYSTEM_ROOT/tmp
-echo "Moving repackaged driver to $SYSTEM_ROOT/tmp..."
-mv "$PATCHED_DRIVER" "$SYSTEM_ROOT/tmp/"
 
 # Blacklist the nouveau driver
 echo "Blacklisting nouveau driver..."
@@ -106,9 +251,13 @@ echo "blacklist nouveau" >"$SYSTEM_ROOT/etc/modprobe.d/disable-nouveau.conf"
 # Disable the nouveau driver
 chroot "$SYSTEM_ROOT" rmmod nouveau || echo "Nouveau module not loaded."
 
-# Run the repackaged driver installer from chroot environment
-echo "Installing the repackaged NVIDIA driver..."
-chroot "$SYSTEM_ROOT" sh "/tmp/$(basename "$PATCHED_DRIVER")" --silent
+# Omit drm dracut module
+vmkdir usr/lib/dracut/dracut.conf.d
+echo "omit_dracutmodules+=\" drm \"" >${SYSTEM_ROOT}/usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+
+# # TODO
+# echo "Regenerating initramfs, please wait..."
+# dracut -f -q --regenerate-all
 
 # Remove the installer and extracted folder after completion
 echo "Cleaning up temporary installation assets..."
